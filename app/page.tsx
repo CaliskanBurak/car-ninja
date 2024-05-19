0a471a8fe957fc58d43fc96bdc636507ec1daee3
CaliskanBurak <caliskan.burakali@gmail.com>
2024-05-18 20:31:57 -0400

    removed console.log

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default async function Home({searchParams}: any) {
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-  console.log(allCars)
 
   return (
     <main className="overflow-hidden">
